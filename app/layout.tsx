3d220a53223e100b130c3df9ec85147931ebf6e0
arcadio <c4net.7@gmail.com>
2023-06-14 15:52:23 +0330

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,18 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "حسین اسکندری | فرانت اند",
   description: "سایت شخصی حسین اسکندری",
+  openGraph: {
+    type: "website",
+    url: "https://rkdo.ir",
+    title: "حسین اسکندری | فرانت اند",
+    description: "وب سایت شخصی حسین اسکندری",
+    siteName: "حسین اسکندری | Frontend Developer",
+    images: [
+      {
+        url: "/images/profile.jpg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
