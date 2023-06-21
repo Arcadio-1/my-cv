71a5f436dcc4983dd23dae7c2886f1b152fb6b2c
arcadio <c4net.7@gmail.com>
2023-06-21 17:40:51 +0330

    change MongoDb access password

--- app/api/util/functions/getClient.ts
+++ app/api/util/functions/getClient.ts
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 export const getClient = async (databaseName: string) => {
   try {
     const client = await MongoClient.connect(
-      `${process.env.REACT_APP_MONGODB}${databaseName}?retryWrites=true&w=majority`
+      `${process.env.Next_APP_MONGODB}${databaseName}?retryWrites=true&w=majority`
     );
     return client;
   } catch (error) {
