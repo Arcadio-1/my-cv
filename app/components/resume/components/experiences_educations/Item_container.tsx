4c0c0a73a09e88a9b4335d9c5e0b64b24b13a9c0
arcadio <c4net.7@gmail.com>
2023-07-18 03:30:32 +0330

    Change style and content
    Experience & educations

--- app/components/resume/components/experiences_educations/Item_container.tsx
+++ app/components/resume/components/experiences_educations/Item_container.tsx
@@ -1,5 +1,7 @@
 import { Experience } from "@/app/util/Types/types";
 import React, { Fragment } from "react";
+import CalenderIcon from "./components/CalenderIcon";
+import LocationIcon from "./components/LocationIcon";
 
 interface Props {
   item: Experience;
@@ -10,12 +12,20 @@ const Item_container = (props: Props) => {
   return (
     <Fragment>
       <p className="tittle">{item.tittle}</p>
-      <div className="date">
-        <span className="date-since">{item.since}</span>
-        <span className="date-dash">-</span>
-        <span className="date-to">{item.to}</span>
+      <div className="content">
+        <div className="content-line">
+          <CalenderIcon />
+          <div className="date">
+            <span className="date-since">{item.since}</span>
+            <span className="date-dash">-</span>
+            <span className="date-to">{item.to}</span>
+          </div>
+        </div>
+        <div className="content-line">
+          <LocationIcon />
+          <span className="place">{item.place}</span>
+        </div>
       </div>
-      <span className="place">{item.place}</span>
     </Fragment>
   );
 };
