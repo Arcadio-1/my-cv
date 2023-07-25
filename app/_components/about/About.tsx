b2fb61e8031237b961f699950edb70aec1330e14
arcadio <c4net.7@gmail.com>
2023-07-25 18:56:26 +0330

    bug has been found finalyyyyyyy
    about components was fucking async function
    this section was use server before and then i change it to client and forget to remove async

--- app/_components/about/About.tsx
+++ app/_components/about/About.tsx
@@ -16,27 +16,31 @@ interface Props {
   lang: Lang;
 }
 
-const About = async (props: Props) => {
+const About = (props: Props) => {
   // console.log(style);
   return (
     <div
       className={`${style.main_about} ${
         props.theme === Theme.Light ? style.main_about_Light : ""
       }
-      ${props.lang === Lang.en ? style.main_about_En : ""}`}
+  ${props.lang === Lang.en ? style.main_about_En : ""}`}
     >
       <SectionHeader
         tag="aboutMe"
         tittle={props.base.section_tittle}
         description={""}
       />
+
       <div className={`${style.main_about_container}`}>
         <Tittle tittle={props.base.tittle} />
+
         <MyImage
           imageUrl={props.personal_info.profile_iamge}
           alt={props.personal_info.name}
         />
+
         <List labels={props.base.labels} personal_info={props.personal_info} />
+
         <Boulshit>{props.about}</Boulshit>
       </div>
     </div>
